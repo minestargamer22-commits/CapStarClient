4b3cfad982c85ac6c086876564c68e780c68f136
minestargamer22-commits <minestargamer22@gmail.com>
2025-10-17 15:07:19 +0330

    Create Menu.java

---
+++ src/main/java/com/capstar/client/Menu.java
@@ -0,0 +1,14 @@
+package com.capstar.client;
+
+public class Menu {
+
+    public static void open() {
+        System.out.println("\n===============================");
+        System.out.println("   §c§lCAP§5§lSTAR §rCLIENT ⚡");
+        System.out.println("===============================");
+        System.out.println("§dFPS: §f120");
+        System.out.println("§dKPS: §f80");
+        System.out.println("§7Shift_Right = Open Menu");
+        System.out.println("===============================");
+    }
+}
