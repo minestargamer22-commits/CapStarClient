d9f824c5901adc3c2e46a79edd16bf205283607c
minestargamer22-commits <minestargamer22@gmail.com>
2025-10-17 15:12:58 +0330

    Create Main2.java

---
+++ Main2.java
@@ -0,0 +1,8 @@
+package com.capstar.client;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("CapStar Client Loaded ✅");
+        Menu.open();
+    }
+}
